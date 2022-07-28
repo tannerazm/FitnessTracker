6d393913deebe45893d4a809f9323e7fa72e13aa
Audrey <audreyhershenson@gmail.com>
2022-07-28 12:49:09 -0500

    finished showing user routines

--- api/users.js
+++ api/users.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const { getPublicRoutinesByUser, getAllRoutines, getAllRoutinesByUser } = require("../db");
+const client = require("../db/client");
 const { JWT_SECRET } = process.env;
 const { createUser, getUserByUsername } = require("../db/users");
 const { requireUser } = require('./utils');
@@ -16,7 +18,7 @@ router.post("/register", async (req, res, next) => {
 
       if (_user) {
         res.status(401)
-        res.send({
+        next({
           error: "USERNAME ALREADY EXISTS",
           message: `User ${username} is already taken.`,
           name: "UserAlreadyExists",
@@ -26,7 +28,7 @@ router.post("/register", async (req, res, next) => {
 
     if (password.length < 8) {
       res.status(401)
-      res.send({
+      next({
         error: "PASSWORD TOO SHORT",
         message: "Password Too Short!",
         name: "PasswordIsTooShort",
@@ -60,9 +62,9 @@ router.post("/login", async (req, res, next) => {
   try {
     const user = await getUserByUsername(username);
     const token = jwt.sign(user, JWT_SECRET)
-    if(user) {
+    if (user) {
       res.send({ token: token, message: "you're logged in!", user: user });
-  
+
       return user;
     }
   } catch (error) {
@@ -73,12 +75,39 @@ router.post("/login", async (req, res, next) => {
 router.get("/me", requireUser, async (req, res, next) => {
   const { user } = req
   try {
-      res.send(user)
-    }
+    res.send(user)
+  }
   catch (error) {
     next(error);
   }
 });
 // GET /api/users/:username/routines
+router.get('/:username/routines', requireUser, async (req, res, next) => {
+  
+  const { username } = req.params
+  const user = await getUserByUsername(username)
+  const joe = await getPublicRoutinesByUser({username: username}
+    ) //any user
+  const bob = await getAllRoutinesByUser({username: username}
+    ) //specific user logged in
+  try {
+    if (!username) {
+      next({
+        error: "PASSWORD TOO SHORT",
+        message: "Password Too Short!",
+        name: "PasswordIsTooShort",
+      });
+    }
+    else if (req.user && user.id === req.user.id) {
+      res.send(bob)
+    }
+    else {
+      res.send(joe)
+    }
+  }
+  catch (error) {
+    next(error);
+  }
+})
 
 module.exports = router;
--- api/utils.js
+++ api/utils.js
@@ -1,6 +1,7 @@
 function requireUser(req, res, next) {
     try {
       if (!req.user) {
+        res.status(401);
         next({
           name: "MissingUserError",
           message: "You must be logged in to perform this action",
